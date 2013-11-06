c9d2cccf2198982c06e80f9df8709c50e256f6ea
Joe Kampschmidt <jokecamp@gmail.com>
2013-11-06 09:46:31 -0500

    Added Network Share Functions

---
+++ NetworkShareFunctions.ps1
@@ -0,0 +1,34 @@
+﻿<#
+    Creates the folder for a network share if it does not exist yet
+    and gives FullControl to the "ANONYMOUS LOGON"
+
+    Example usage: 
+        Create-ShareWithAnonFullControl 'E:\Shares' 'Shares'
+#>
+Function Create-ShareWithAnonFullControl ([String]$FullPath, [String]$ShareName)
+{
+    #alias the command
+    $Shares=[WMICLASS]'WIN32_Share'
+
+    IF (!(TEST-PATH $FullPath)) {
+        New-Item -type directory -Path $FullPath
+    }
+
+    $Shares.Create($FullPath,$ShareName,0)
+    $Acl = Get-Acl $FullPath
+    $Ar = New-Object system.security.accesscontrol.filesystemaccessrule('ANONYMOUS LOGON','FullControl','ContainerInherit, ObjectInherit', 'None', 'Allow')
+    $Acl.AddAccessRule($Ar)
+    Set-Acl $FullPath $Acl
+}
+
+
+<# 
+    List all the existing shares on this computer. This is useful to verify 
+    the shares you have created.
+
+    Example usage:
+        Get-AllShares
+#>
+Function Get-AllShares {
+    GWMI win32_share | ft name, path, description -auto
+}
--- README.md
+++ README.md
@@ -2,3 +2,9 @@ PowerShell.ToolBelt
 ===================
 
 A various collection of powershell scripts to aid a devops engineer.
+
+Table of Contents
+
+- Network Share Functions
+ - Get-AllShares
+ - Create-ShareWithAnonFullControl
