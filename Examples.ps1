6315ddd727d42d9a87ddb61bf3d0c3be933c4445
Joe Kampschmidt <jokecamp@gmail.com>
2014-01-10 15:15:27 -0500

    Added JSON parsing example

---
+++ Examples.ps1
@@ -0,0 +1,10 @@
+﻿<#
+    Learning Aid - a showcase of very simple examples of useful tasks to perform
+    via powershell. Helpful for those learning powershell
+
+#>
+
+# Sending an email
+$smtp = new-object Net.Mail.SmtpClient('mail.test.org') 
+$smtp.Send('from@test.org', 'to@test.org', 'Subject', 'body') 
+
---
+++ ParseJson.ps1
@@ -0,0 +1,7 @@
+﻿<# Read Json - via http://stackoverflow.com/a/17602226/215502 #>
+
+[System.Reflection.Assembly]::LoadWithPartialName("System.Web.Extensions")
+$json = "{a:1,b:2,c:{nested:true}}"
+$ser = New-Object System.Web.Script.Serialization.JavaScriptSerializer
+$obj = $ser.DeserializeObject($json)
+write-output $obj
